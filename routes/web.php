bd37642c3d534ff3f0d40b19eab5a513075d91dc
joshua-belanger <joshua_belanger@hotmail.com>
2023-01-24 12:55:56 -0500

    Changes rate limit to 10:10

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WebCrawlerController;
 
-Route::middleware(['throttle:5,10'])->group(function () {
+Route::middleware(['throttle:10,10'])->group(function () {
     Route::get('/', [WebCrawlerController::class, 'index']);
     Route::get('scrape', [WebCrawlerController::class, 'index']);
 });
